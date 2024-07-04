2916e072fe72e3566ea481435515204c56edb9fe
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2024-07-04 14:33:16 +0530

    Made provision to add more implementations

---
+++ assets/default/default.js
@@ -0,0 +1,3 @@
+document.getElementById("video").addEventListener("pause", (e) => {
+    console.log("Video has been paused at: ", e);
+});
--- server/routers/router.go
+++ server/routers/router.go
@@ -18,18 +18,38 @@ type VideoPath struct {
 func InitializeAllRoutes(wd string) *mux.Router {
 	fmt.Println("Initializing Routers")
 	r := mux.NewRouter()
-	r.HandleFunc("/", sayHi)
+	r.HandleFunc("/", indexPage)
+	r.HandleFunc("/default", defaultImplementation)
+	r.HandleFunc("/custom", customImeplementation)
+
 	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	return r
 }
 
-func sayHi(w http.ResponseWriter, r *http.Request) {
+func indexPage(w http.ResponseWriter, r *http.Request) {
 	wd, _ := os.Getwd()
 	// fmt.Println("Working Dir is: ", wd)
 	vp := VideoPath{VideoPath: "/assets/BBB-Test-Video.mp4", ImgPath: "/assets/linux-test-img.png"}
 	// vp := VideoPath{VideoPath: "https://www.w3schools.com/html/mov_bbb.mp4"}
 
 	indexFilePath := filepath.Join(wd, "frontend", "index.html")
-	hiTemplate := template.Must(template.ParseFiles(indexFilePath))
-	hiTemplate.Execute(w, vp)
+	template.Must(template.ParseFiles(indexFilePath)).Execute(w, vp)
+}
+
+func defaultImplementation(w http.ResponseWriter, r *http.Request) {
+	wd, _ := os.Getwd()
+
+	vp := VideoPath{VideoPath: "/assets/BBB-Test-Video.mp4", ImgPath: "/assets/linux-test-img.png"}
+	defaultImplementationPath := filepath.Join(wd, "frontend", "default", "default.html")
+
+	template.Must(template.ParseFiles(defaultImplementationPath)).Execute(w, vp)
+}
+
+func customImeplementation(w http.ResponseWriter, r *http.Request) {
+	wd, _ := os.Getwd()
+
+	vp := VideoPath{VideoPath: "/assets/BBB-Test-Video.mp4", ImgPath: "/assets/linux-test-img.png"}
+	customImplementationPath := filepath.Join(wd, "frontend", "custom", "custom.html")
+
+	template.Must(template.ParseFiles(customImplementationPath)).Execute(w, vp)
 }
