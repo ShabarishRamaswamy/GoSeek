0765819e61a7e611ec778a7751460eb8c2af0392
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2024-07-08 20:15:42 +0530

    Now works on both browsers

--- server/routers/router.go
+++ server/routers/router.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"text/template"
-	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -60,116 +60,60 @@ func customImeplementation(w http.ResponseWriter, r *http.Request) {
 	template.Must(template.ParseFiles(customImplementationPath)).Execute(w, vp)
 }
 
+// TODOS;
+// Errors when reading files.
+// Errors when parsing ranges. [File Sizes, If Ranges are not correctly formatted, If no range is present].
 func serveCustom(w http.ResponseWriter, r *http.Request) {
 	wd, _ := os.Getwd()
 	fmt.Printf("Got the Request %+v\n", r)
 	filePath := filepath.Join(wd, "assets", "BBB-Test-Video.mp4")
 
-	fmt.Printf("\nRequest headers: %+v\n", r.Header)
+	// fmt.Printf("\nRequest headers: %+v\n", r.Header)
 
 	videoFile, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println("ERROR: ", err.Error())
 	}
+	defer videoFile.Close()
+
 	videoFileProperties, err := videoFile.Stat()
 	if err != nil {
 		fmt.Println("ERROR: ", err.Error())
 	}
 
-	fmt.Printf("\nRange headers: %+v\n", r.Header["Range"])
+	// fmt.Printf("\nRange headers: %+v\n", r.Header["Range"])
 	requestRangeUnprocessed := r.Header["Range"]
-	reqRange := strings.Split(requestRangeUnprocessed[0], "=")
-	reqRange = strings.Split(reqRange[1], "-")
-	fmt.Println("Request Range: ", reqRange)
-	if reqRange[0] == "0" {
-		fmt.Println("Initial Request")
+	var reqRange []string
+	var vidSeekStart, vidSeekEnd int
+
+	if requestRangeUnprocessed[0] != "" {
+		reqRange = strings.Split(requestRangeUnprocessed[0], "=")
+		reqRange = strings.Split(reqRange[1], "-")
+		// fmt.Println("Request Range: ", reqRange)
+
+		vidSeekStart, _ = strconv.Atoi(reqRange[0])
+		if reqRange[0] == "0" || reqRange[1] == "" {
+			// fmt.Println("Initial Request")
+			vidSeekEnd = int(videoFileProperties.Size()) - 1
+		} else {
+			vidSeekEnd, _ = strconv.Atoi(reqRange[1])
+		}
 	}
 
-	w.Header().Add("Content-Range", fmt.Sprintf("bytes 0-%d/%d", videoFileProperties.Size(), videoFileProperties.Size()))
-	w.Header().Add("Content-Length", fmt.Sprintf("%d", videoFileProperties.Size()))
+	// fmt.Println("Seek Properties: ", vidSeekStart, vidSeekEnd)
+
+	vidSeekSize := vidSeekEnd - vidSeekStart + 1
+
+	w.Header().Add("Content-Range", fmt.Sprintf("bytes %d-%d/%d", vidSeekStart, vidSeekEnd, videoFileProperties.Size()))
+	w.Header().Add("Content-Length", fmt.Sprintf("%d", vidSeekSize))
 	w.Header().Add("Content-Type", "video/mp4")
-	w.Header().Add("Last-Modified", fmt.Sprintf("%+v", time.Now()))
+	w.Header().Add("Last-Modified", videoFileProperties.ModTime().UTC().Format(http.TimeFormat))
 	w.Header().Add("Accept-Ranges", "bytes")
 	w.WriteHeader(http.StatusPartialContent)
 
-	var sendContent io.Reader = videoFile
-	io.CopyN(w, sendContent, videoFileProperties.Size())
-	// fmt.Printf("%+v", w.Header())
+	videoFile.Seek(int64(vidSeekStart), 0)
+	io.CopyN(w, videoFile, int64(vidSeekSize))
 }
-
-// func serveCustom(w http.ResponseWriter, r *http.Request) {
-// 	wd, _ := os.Getwd()
-// 	fmt.Printf("Got the Request %+v\n", r)
-// 	filePath := filepath.Join(wd, "assets", "BBB-Test-Video.mp4")
-
-// 	fmt.Printf("\nRequest headers: %+v\n", r.Header)
-// 	fmt.Printf("\nRange headers: %+v\n", r.Header["Range"])
-
-// 	videoFile, err := os.Open(filePath)
-// 	if err != nil {
-// 		http.Error(w, "File not found.", 404)
-// 		return
-// 	}
-// 	defer videoFile.Close()
-
-// 	videoFileProperties, err := videoFile.Stat()
-// 	if err != nil {
-// 		http.Error(w, "Could not get file properties.", 500)
-// 		return
-// 	}
-
-// 	fileSize := videoFileProperties.Size()
-// 	rangeHeader := r.Header.Get("Range")
-
-// 	if rangeHeader != "" {
-// 		// Parse the Range header to determine the start and end byte positions
-// 		rangeParts := strings.Split(strings.TrimPrefix(rangeHeader, "bytes="), "-")
-// 		start, err := strconv.ParseInt(rangeParts[0], 10, 64)
-// 		if err != nil {
-// 			http.Error(w, "Invalid range start.", 400)
-// 			return
-// 		}
-
-// 		var end int64
-// 		if len(rangeParts) > 1 && rangeParts[1] != "" {
-// 			end, err = strconv.ParseInt(rangeParts[1], 10, 64)
-// 			if err != nil {
-// 				http.Error(w, "Invalid range end.", 400)
-// 				return
-// 			}
-// 		} else {
-// 			end = fileSize - 1
-// 		}
-
-// 		if start > end || end >= fileSize {
-// 			http.Error(w, "Invalid range.", 416)
-// 			return
-// 		}
-
-// 		rangeLength := end - start + 1
-
-// 		w.Header().Add("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, fileSize))
-// 		w.Header().Add("Content-Length", fmt.Sprintf("%d", rangeLength))
-// 		w.Header().Add("Content-Type", "video/mp4")
-// 		w.Header().Add("Last-Modified", videoFileProperties.ModTime().UTC().Format(http.TimeFormat))
-// 		w.Header().Add("Accept-Ranges", "bytes")
-// 		w.WriteHeader(http.StatusPartialContent)
-
-// 		// Serve the requested range
-// 		videoFile.Seek(start, 0)
-// 		io.CopyN(w, videoFile, rangeLength)
-// 	} else {
-// 		// Serve the entire file if no range is requested
-// 		w.Header().Add("Content-Length", fmt.Sprintf("%d", fileSize))
-// 		w.Header().Add("Content-Type", "video/mp4")
-// 		w.Header().Add("Last-Modified", videoFileProperties.ModTime().UTC().Format(http.TimeFormat))
-// 		w.Header().Add("Accept-Ranges", "bytes")
-// 		w.WriteHeader(http.StatusOK)
-// 		io.Copy(w, videoFile)
-// 	}
-
-// 	fmt.Printf("%+v", w.Header())
-// }
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
